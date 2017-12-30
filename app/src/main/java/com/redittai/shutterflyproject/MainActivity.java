d3dbe7558ebfe85477c89d00dfdfc3752a408ab0
redittai <redittai@gmial.com>
2017-12-30 21:11:35 +0200

    Optimizations & Documentations

--- app/src/main/java/com/redittai/shutterflyproject/MainActivity.java
+++ app/src/main/java/com/redittai/shutterflyproject/MainActivity.java
@@ -2,9 +2,7 @@ package com.redittai.shutterflyproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 import com.redittai.shutterflyproject.Models.pitPoint;
@@ -23,50 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        pit = findViewById(R.id.CustomComponent);
+        this.pit = findViewById(R.id.CustomComponent);
         addPit = findViewById(R.id.addPitBTN);
-        pit.getWidth();
-        pit.getHeight();
-        if (IS_FIRST_ENTER){
-            initFirstPitPoints();
-            IS_FIRST_ENTER = false;
-        }
+
         addPit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               addaNewPitPoint();
+                ((mCanvas)pit).createNewPitPoint();
+
             }
         });
 
     }
-    // used to determine the position of the initialized pitPoint
-    private float getScreenWidth(){
-        DisplayMetrics dm = new DisplayMetrics();
-        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(WINDOW_SERVICE);
-        wm.getDefaultDisplay().getMetrics(dm);
-        int widthInPixel = Math.round(dm.widthPixels);
-        return widthInPixel;
-    }
-    // add a new pit point to the view group
-    private void addaNewPitPoint(){
-        pitPoint p = new pitPoint(getApplicationContext(),pit.getWidth()/2 , pit.getHeight() / 2);
-        com.redittai.shutterflyproject.Views.mCanvas.pitPoints.add(p);
-        pit.invalidate();
-    }
 
-    private void initFirstPitPoints() {
-        int step = 100;
-        for (int i = 0; i < 5; i++) {
-            Random r = new Random();
-            int x = r.nextInt(1000);
-            int y = r.nextInt(1080);
+    /**
+     * Check if on resume is called for the first time.
+     * If it is create 5 pitPoints.
+     * Called from mCanvas.class
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-            pitPoint p1 = new pitPoint(getApplicationContext(),x, y);
-
-
-            mCanvas.pitPoints.add(p1);
-
+        if (IS_FIRST_ENTER){
+            ((mCanvas)pit). initFirstPitPoints();
+            IS_FIRST_ENTER = false;
         }
 
     }
+
+
+
 }
